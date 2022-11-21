7d6b5e852770192a424f344b0b9506fae0bb3eeb
Ethan Willers <willers.enw@gmail.com>
2022-11-20 19:23:19 -0800

    finnished save notes feature

--- Develop/script.js
+++ Develop/script.js
@@ -19,6 +19,9 @@ $(function () {
   }
   if (localStorage.getItem("textBoxes")) {
     textByTime = JSON.parse(localStorage.getItem("textBoxes"));
+    for (var x = 1; x < 25; x++) {
+      $('#hour-' + (x)).children().eq(1).val(textByTime[x]);
+    }
   }
   
   containerDiv.click(function (event) {
@@ -36,16 +39,18 @@ $(function () {
       localStorage.setItem("textBoxes", JSON.stringify(textByTime));
     }
   })
-
-  function fillText(obj) {
-    // Fills text boxes
-  }
   //
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
+  var hour = dayjs().format('H');
+  $('#hour-' + hour).addClass('present');
+
+  for (var x = hour; x < 24; x++) {
+    
+  }
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
