08a18d12c6147d6c75f79ee6926f7e9b28815f21
Ethan Willers <willers.enw@gmail.com>
2022-11-20 18:49:36 -0800

    added local storage saving

--- Develop/script.js
+++ Develop/script.js
@@ -2,12 +2,44 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
+  var containerDiv = $('body').children().eq(1).children();
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
+  var textByTime = {
+    // AM
+    1: "", 2: "", 3: "", 4: "", 5 : "", 6 : "",
+    7: "", 8: "", 9: "", 10: "", 11: "", 12: "",
+    // PM
+    13: "", 14: "", 15: "", 16: "", 17: "", 18: "",
+    19: "", 20: "", 21: "", 22: "", 23: "", 24: ""
+  }
+  if (localStorage.getItem("textBoxes")) {
+    textByTime = JSON.parse(localStorage.getItem("textBoxes"));
+  }
+  
+  containerDiv.click(function (event) {
+    var el = event.target;
+    var id = event.currentTarget.getAttribute("id");
+    var textBox = $('#' + id).children().eq(1);
+
+    if (el.matches("i") || el.matches("button")) {
+      var num;
+      if (id.length > 6) {
+        num = id.slice(-2);
+      } else num = id.slice(-1);
+
+      textByTime[num] = textBox.val();
+      localStorage.setItem("textBoxes", JSON.stringify(textByTime));
+    }
+  })
+
+  function fillText(obj) {
+    // Fills text boxes
+  }
   //
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
